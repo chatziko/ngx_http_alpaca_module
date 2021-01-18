2d505322e7008dcb01049f1be718c7bf7970ca65
Lefteris <lefte1998di@gmail.com>
2021-01-18 13:56:31 +0200

    added probablilistic fake object check and fixed bugs

--- testing_ALPaCA/testing.py
+++ testing_ALPaCA/testing.py
@@ -5,14 +5,15 @@ from utils            import colors
 from response_handler import get_response_filenames
 
 methods = {
-    'deter_simple'      : '/Deterministic/nginx_simple.conf',
-    'deter_fake_imgs'   : '/Deterministic/nginx_fake_imgs.conf',
-    'deter_inline_all'  : '/Deterministic/nginx_inline_all.conf',
-    'deter_inline_some' : '/Deterministic/nginx_inline_some.conf',
+    # 'deter_simple'      : '/Deterministic/nginx_simple.conf',
+    # 'deter_fake_imgs'   : '/Deterministic/nginx_fake_imgs.conf',
+    # 'deter_inline_all'  : '/Deterministic/nginx_inline_all.conf',
+    # 'deter_inline_some' : '/Deterministic/nginx_inline_some.conf',
 
-    'prob_simple'       : '/Probabilistic/nginx_simple.conf',
-    'prob_inline_all'   : '/Probabilistic/nginx_inline_all.conf',
-    'prob_inline_some'  : '/Probabilistic/nginx_inline_some.conf',
+    # 'prob_simple'       : '/Probabilistic/nginx_simple.conf',
+    'prob_fake_imgs'    : '/Probabilistic/nginx_fake_imgs.conf',
+    # 'prob_inline_all'   : '/Probabilistic/nginx_inline_all.conf',
+    # 'prob_inline_some'  : '/Probabilistic/nginx_inline_some.conf',
 }
 
 inlines = {
@@ -22,6 +23,7 @@ inlines = {
     'deter_inline_some' : 2,
 
     'prob_simple'       : 0,
+    'prob_fake_imgs'    : 0,
     'prob_inline_all'   : 3,
     'prob_inline_some'  : 2,
 }
@@ -33,6 +35,7 @@ fake_imgs = {
     'deter_inline_some' : 0,
 
     'prob_simple'       : 2,
+    'prob_fake_imgs'    : [1,2,3,4],
     'prob_inline_all'   : 0,
     'prob_inline_some'  : 0,
 }
@@ -91,9 +94,15 @@ if __name__ == "__main__":
                 print("Inlining error in {}. Expected {} inlined objects and received {}.".format(conf_name, inlines[conf_name], inl_num))
                 success = False
 
-            if success == True and fake_img_num != fake_imgs[conf_name]:
+            if not isinstance(fake_imgs[conf_name],list):
+                if success == True and fake_img_num != fake_imgs[conf_name]:
+                    print("Fake images error in {}. Expected {} fake images and received {}.".format(conf_name, fake_imgs[conf_name], fake_img_num))
+                    success = False
+
+            elif success == True and fake_img_num not in fake_imgs[conf_name]:
                 print("Fake images error in {}. Expected {} fake images and received {}.".format(conf_name, fake_imgs[conf_name], fake_img_num))
                 success = False
+
 
         print("{:17} : {}".format(conf_name, success_msg[success]))
 
